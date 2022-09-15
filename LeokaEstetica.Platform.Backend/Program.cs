ec36726a4fd01b45f9ae8e72e2d7a64c8c5ceba7
Oyarsa <sierra_93@mail.ru>
2022-09-16 01:23:17 +0300

    Проверка конфигурации в тимсити и на стенде дева.

--- LeokaEstetica.Platform.Backend/Program.cs
+++ LeokaEstetica.Platform.Backend/Program.cs
@@ -4,13 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 builder.Services.AddControllers().AddControllersAsServices();
-builder.Services.AddCors(options => options.AddPolicy("ApiCorsPolicy", b =>
-{
-    b.WithOrigins(configuration.GetSection("CorsUrls:Urls").Get<string[]>())
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials();
-}));
+// builder.Services.AddCors(options => options.AddPolicy("ApiCorsPolicy", b =>
+// {
+//     b.WithOrigins(configuration.GetSection("CorsUrls:Urls").Get<string[]>())
+//         .AllowAnyHeader()
+//         .AllowAnyMethod()
+//         .AllowCredentials();
+// }));
 
 builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "Leoka.Estetica.Platform" }); });
 
