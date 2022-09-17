83a927daca6a73c05cf706269f4960f56b70ace5
Oyarsa <sierra_93@mail.ru>
2022-09-17 17:53:07 +0300

    [#2zaa4nw] Завел сущность для таблицы фона.

---
+++ LeokaEstetica.Platform.Models/Entities/Landing/FonEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace LeokaEstetica.Platform.Models.Entities.Landing;
+
+/// <summary>
+/// Класс сопоставляется с таблицей dbo.Fon.
+/// </summary>
+public class FonEntity
+{
+    /// <summary>
+    /// PK.
+    /// </summary>
+    public int FonId { get; set; }
+
+    /// <summary>
+    /// Заголовок фона.
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    /// Описание фона.
+    /// </summary>
+    public string Text { get; set; }
+}
