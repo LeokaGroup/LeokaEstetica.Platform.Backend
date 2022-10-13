b647a252b1f5875b065dd63814053ce873795b8d
Oyarsa <sierra_93@mail.ru>
2022-10-13 21:23:45 +0300

    [#319a1b1] Добавил ендпоинты и сервис профиля.

---
+++ LeokaEstetica.Platform.Services/Services/User/ProfileService.cs
@@ -0,0 +1,11 @@
+using LeokaEstetica.Platform.Services.Abstractions.User;
+
+namespace LeokaEstetica.Platform.Services.Services.User;
+
+/// <summary>
+/// Класс реализует методы сервиса профиля пользователя.
+/// </summary>
+public sealed class ProfileService : IProfileService
+{
+    
+}
