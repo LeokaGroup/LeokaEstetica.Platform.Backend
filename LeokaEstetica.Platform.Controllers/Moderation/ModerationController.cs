3c2a76d49e805cc6b447670254d068a664636092
Timoshenko A.I <sierra_93@mail.ru>
2022-12-21 22:36:42 +0300

    [feature/add-moderation-auth-logic] Добавляю логику проверки роли для допуска к модерации.

---
+++ LeokaEstetica.Platform.Controllers/Moderation/ModerationController.cs
@@ -0,0 +1,15 @@
+using LeokaEstetica.Platform.Base;
+
+namespace LeokaEstetica.Platform.Controllers.Moderation;
+
+/// <summary>
+/// Контроллер модерации (отвечает за весь функционал модерации).
+/// </summary>
+public class ModerationController : BaseController
+{
+    public ModerationController()
+    {
+    }
+    
+    
+}
