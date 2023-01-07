923d1d7444be5d1e2c7a58fcceaaf4c647976a05
Timoshenko A.I <sierra_93@mail.ru>
2023-01-07 16:17:28 +0300

    [feature/filter-vacancies] Добавил фильтр по опыту работы (не имеет значения).

---
+++ LeokaEstetica.Platform.LuceneNet/Chains/Vacancy/UnknownExperienceVacanciesFilterChain.cs
@@ -0,0 +1,40 @@
+using LeokaEstetica.Platform.Core.Extensions;
+using LeokaEstetica.Platform.LuceneNet.Builders;
+using LeokaEstetica.Platform.LuceneNet.Consts;
+using LeokaEstetica.Platform.Models.Dto.Input.Vacancy;
+using LeokaEstetica.Platform.Models.Dto.Output.Vacancy;
+using LeokaEstetica.Platform.Models.Enums;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+
+namespace LeokaEstetica.Platform.LuceneNet.Chains.Vacancy;
+
+/// <summary>
+/// Класс фильтра по опыту работу (не имеет значения).
+/// </summary>
+public class UnknownExperienceVacanciesFilterChain : BaseVacanciesFilterChain
+{
+    public override async Task<IQueryable<CatalogVacancyOutput>> FilterVacanciesAsync(FilterVacancyInput filters,
+        IOrderedQueryable<CatalogVacancyOutput> vacancies)
+    {
+        // Если фильтр опыта работы не (без оплаты), то передаем следующему по цепочке.
+        if (Enum.Parse<FilterExperienceTypeEnum>(filters.Experience) != FilterExperienceTypeEnum.Unknown)
+        {
+            return await CallNextSuccessor(filters, vacancies);
+        }
+
+        using var reader = IndexReader.Open(_index.Value, true);
+        using var searcher = new IndexSearcher(reader);
+        var filterQuery =
+            new TermQuery(new Term(VacancyFinderConst.WORK_EXPERIENCE,
+                FilterExperienceTypeEnum.Unknown.GetEnumDescription()));
+        var filter = new QueryWrapperFilter(filterQuery);
+
+        // Больше 20 и не надо, так как есть пагинация.
+        var searchResults = searcher.Search(new MatchAllDocsQuery(), filter, 20).ScoreDocs;
+        var result = CreateVacanciesSearchResultBuilder.CreateVacanciesSearchResult(searchResults, searcher);
+        vacancies = (IOrderedQueryable<CatalogVacancyOutput>)result;
+
+        return await CallNextSuccessor(filters, vacancies);
+    }
+}
