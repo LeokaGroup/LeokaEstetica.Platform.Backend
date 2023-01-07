8423b9f1416aea8e036a114c9bf9ea13bb4551cc
Timoshenko A.I <sierra_93@mail.ru>
2023-01-07 17:14:32 +0300

    [feature/filter-vacancies] Добавил фильтр по опыту работы (более 6 лет).

---
+++ LeokaEstetica.Platform.LuceneNet/Chains/Vacancy/ManySixExperienceVacanciesFilterChain.cs
@@ -0,0 +1,45 @@
+using LeokaEstetica.Platform.Core.Extensions;
+using LeokaEstetica.Platform.LuceneNet.Builders;
+using LeokaEstetica.Platform.LuceneNet.Consts;
+using LeokaEstetica.Platform.Models.Dto.Input.Vacancy;
+using LeokaEstetica.Platform.Models.Dto.Output.Vacancy;
+using LeokaEstetica.Platform.Models.Enums;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+
+namespace LeokaEstetica.Platform.LuceneNet.Chains.Vacancy;
+
+/// <summary>
+/// Класс фильтра по опыту работу (более 6 лет).
+/// </summary>
+public class ManySixExperienceVacanciesFilterChain : BaseVacanciesFilterChain
+{
+    /// <summary>
+    /// Метод фильтрует вакансии по опыту работы (более 6 лет).
+    /// </summary>
+    /// <param name="filters">Условия фильтрации.</param>
+    /// <param name="vacancies">Список вакансий до фильтрации без выгрузки в память.</param>
+    /// <returns>Список вакансий после фильтрации.</returns>
+    public override async Task<IQueryable<CatalogVacancyOutput>> FilterVacanciesAsync(FilterVacancyInput filters, IOrderedQueryable<CatalogVacancyOutput> vacancies)
+    {
+        // Если фильтр опыта работы не (от 3 до 6 лет), то передаем следующему по цепочке.
+        if (Enum.Parse<FilterExperienceTypeEnum>(filters.Experience) != FilterExperienceTypeEnum.ManySix)
+        {
+            return await CallNextSuccessor(filters, vacancies);
+        }
+
+        using var reader = IndexReader.Open(_index.Value, true);
+        using var searcher = new IndexSearcher(reader);
+        var filterQuery =
+            new TermQuery(new Term(VacancyFinderConst.WORK_EXPERIENCE,
+                FilterExperienceTypeEnum.ManySix.GetEnumDescription()));
+        var filter = new QueryWrapperFilter(filterQuery);
+
+        // Больше 20 и не надо, так как есть пагинация.
+        var searchResults = searcher.Search(new MatchAllDocsQuery(), filter, 20).ScoreDocs;
+        var result = CreateVacanciesSearchResultBuilder.CreateVacanciesSearchResult(searchResults, searcher);
+        vacancies = (IOrderedQueryable<CatalogVacancyOutput>)result;
+
+        return await CallNextSuccessor(filters, vacancies);
+    }
+}
