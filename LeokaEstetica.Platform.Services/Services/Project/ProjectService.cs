0c6ba599be4ba6af268e61968ef58d8256e35b2c
Timoshenko A.I <sierra_93@mail.ru>
2023-02-09 01:46:52 +0300

    [feature/selected-color-subscription-catalogs] Фикс юсинга.

--- LeokaEstetica.Platform.Services/Services/Project/ProjectService.cs
+++ LeokaEstetica.Platform.Services/Services/Project/ProjectService.cs
@@ -4,7 +4,6 @@ using LeokaEstetica.Platform.Access.Enums;
 using LeokaEstetica.Platform.Core.Constants;
 using LeokaEstetica.Platform.Core.Helpers;
 using LeokaEstetica.Platform.Core.Exceptions;
-using LeokaEstetica.Platform.Core.Extensions;
 using LeokaEstetica.Platform.Database.Abstractions.FareRule;
 using LeokaEstetica.Platform.Database.Abstractions.Project;
 using LeokaEstetica.Platform.Database.Abstractions.Subscription;
