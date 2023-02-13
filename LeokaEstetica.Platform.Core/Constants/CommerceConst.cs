1a8a743d8618af1790a477fd6bf486616dbc7e08
Timoshenko A.I <sierra_93@mail.ru>
2023-02-13 20:52:10 +0300

    Удалил лишние ключи.

--- LeokaEstetica.Platform.Core/Constants/CommerceConst.cs
+++ LeokaEstetica.Platform.Core/Constants/CommerceConst.cs
@@ -6,16 +6,6 @@ namespace LeokaEstetica.Platform.Core.Constants;
 public static class CommerceConst
 {
     /// <summary>
-    /// Если не заполнен объект с ценой.
-    /// </summary>
-    public const string EMPTY_AMOUNT = "Данные цены не могут быть пустыми.";
-    
-    /// <summary>
-    /// Если не заполнен объект счета.
-    /// </summary>
-    public const string EMPTY_INVOICE = "Данные счета не могут быть пустыми.";
-
-    /// <summary>
     /// Если невалидный Id тарифа.
     /// </summary>
     public const string NOT_VALID_FARE_RULE_ID = "Невалидный Id тарифа";
