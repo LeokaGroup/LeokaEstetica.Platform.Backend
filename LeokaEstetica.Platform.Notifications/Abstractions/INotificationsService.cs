33968881a5cd3f57290fe4630e6d06e552fcd52e
Timoshenko A.I <sierra_93@mail.ru>
2023-02-22 00:38:48 +0300

    [feature/remove-selected-skills] Фикс сохранения добавленных либо удаленных навыков пользователя.

--- LeokaEstetica.Platform.Notifications/Abstractions/INotificationsService.cs
+++ LeokaEstetica.Platform.Notifications/Abstractions/INotificationsService.cs
@@ -12,27 +12,4 @@ public interface INotificationsService
     /// <param name="notifyText">Текст уведомления.</param>
     /// <param name="notificationLevel">Уровень уведомления.</param>
     Task SendNotifySuccessSaveAsync(string title, string notifyText, string notificationLevel);
-
-    /// <summary>
-    /// Метод сохраняет ConnectionId подключения SignalR в кэш.
-    /// </summary>
-    /// <param name="connectionId">Id подключения, который создает SignalR.</param>
-    /// <param name="userCode">Код пользователя.</param>
-    // Task SaveConnectionIdCacheAsync(string connectionId, string userCode);
-
-    /// <summary>
-    /// Метод отправляет уведомление с предупреждением о пустом списке навыков пользователя. Пользователь значит не выбрал навыки.
-    /// </summary>
-    /// <param name="title">Заголовок уведомления.</param>
-    /// <param name="notifyText">Текст уведомления.</param>
-    /// <param name="notificationLevel">Уровень уведомления.</param>
-    Task SendNotificationWarningSaveUserSkillsAsync(string title, string notifyText, string notificationLevel);
-    
-    /// <summary>
-    /// Метод отправляет уведомление с предупреждением о пустом списке целей пользователя. Пользователь значит не выбрал навыки.
-    /// </summary>
-    /// <param name="title">Заголовок уведомления.</param>
-    /// <param name="notifyText">Текст уведомления.</param>
-    /// <param name="notificationLevel">Уровень уведомления.</param>
-    Task SendNotificationWarningSaveUserIntentsAsync(string title, string notifyText, string notificationLevel);
 }
