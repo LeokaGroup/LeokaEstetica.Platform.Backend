668129d9b6e4d11e1c6e0b14b3fd9c717be2f87b
Timoshenko A.I <sierra_93@mail.ru>
2023-03-13 01:25:34 +0300

    [feature/get-timelines] Добавил маппинги и сущность.

---
+++ LeokaEstetica.Platform.Models/Mappings/Landing/TimelineConfiguration.cs
@@ -0,0 +1,46 @@
+using LeokaEstetica.Platform.Models.Entities.Landing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LeokaEstetica.Platform.Models.Mappings.Landing;
+
+public partial class TimelineConfiguration : IEntityTypeConfiguration<TimelineEntity>
+{
+    public void Configure(EntityTypeBuilder<TimelineEntity> entity)
+    {
+        entity.ToTable("Timelines", "dbo");
+
+        entity.HasKey(e => e.TimelineId);
+
+        entity.Property(e => e.TimelineId)
+            .HasColumnName("TimelineId")
+            .HasColumnType("serial");
+
+        entity.Property(e => e.TimelineText)
+            .HasColumnName("TimelineText")
+            .HasColumnType("text")
+            .IsRequired();
+
+        entity.Property(e => e.TimelineTitle)
+            .HasColumnName("TimelineTitle")
+            .HasColumnType("varchar(200)")
+            .IsRequired();
+        
+        entity.Property(e => e.TimelineSysType)
+            .HasColumnName("TimelineSysType")
+            .HasColumnType("varchar(150)")
+            .IsRequired();
+        
+        entity.Property(e => e.TimelineTypeName)
+            .HasColumnName("TimelineTypeName")
+            .HasColumnType("varchar(150)");
+
+        entity.HasIndex(u => u.TimelineId)
+            .HasDatabaseName("PK_TimelineId")
+            .IsUnique();
+
+        OnConfigurePartial(entity);
+    }
+
+    partial void OnConfigurePartial(EntityTypeBuilder<TimelineEntity> entity);
+}
