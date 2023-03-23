1c422ada35bb51e4743b0860847aade47fc5dde5
Lev <leva.100s@gmail.com>
2023-03-23 16:28:44 +0300

    Добавлена сущность и маппинг для таблицы архива проектов

---
+++ LeokaEstetica.Platform.Models/Entities/Project/ArchivedProjectEntity.cs
@@ -0,0 +1,27 @@
+﻿namespace LeokaEstetica.Platform.Models.Entities.Project;
+
+/// <summary>
+/// Класс сопоставляется с таблицей архива проектов Projects.ArchivedProjects.
+/// </summary>
+public class ArchivedProjectEntity
+{
+    /// <summary>
+    /// PK.
+    /// </summary>
+    public long ArchiveId { get; set; }
+
+    /// <summary>
+    /// Id проекта.
+    /// </summary>
+    public long ProjectId { get; set; }
+
+    /// <summary>
+    /// Дата архива.
+    /// </summary>
+    public DateTime DateArchived { get; set; }
+
+    /// <summary>
+    /// FK на проекты пользователя.
+    /// </summary>
+    public UserProjectEntity UserProject { get; set; }
+}
