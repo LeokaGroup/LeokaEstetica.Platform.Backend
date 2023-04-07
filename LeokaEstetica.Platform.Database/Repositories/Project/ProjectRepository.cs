f8dace4eaf9495268e3b75b4ae26ef7ea22b0aaa
Lev <leva.100s@gmail.com>
2023-04-07 21:12:59 +0300

    #773 Добавлена проверка архива проектов перед формированием каталога проектов.

--- LeokaEstetica.Platform.Database/Repositories/Project/ProjectRepository.cs
+++ LeokaEstetica.Platform.Database/Repositories/Project/ProjectRepository.cs
@@ -183,6 +183,8 @@ public class ProjectRepository : IProjectRepository
     {
         var result = await _pgContext.CatalogProjects
             .Include(p => p.Project)
+            .Where(p => !p.Project.ArchivedProjects
+            .Any(a => a.ProjectId == p.ProjectId))
             .Select(p => new CatalogProjectOutput
             {
                 ProjectId = p.Project.ProjectId,
