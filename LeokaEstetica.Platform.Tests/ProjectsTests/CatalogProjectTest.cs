9f8796e2839a7bb765f968fb3a6c04f2d59c7721
Lev <leva.100s@gmail.com>
2023-04-10 17:47:46 +0300

    #773 Добавлен тест

---
+++ LeokaEstetica.Platform.Tests/ProjectsTests/CatalogProjectTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeokaEstetica.Platform.Tests.ProjectsTests;
+
+[TestFixture]
+public class CatalogProjectTest : BaseServiceTest
+{
+    [Test]
+    public async Task GetCatalogProjectAsyncTest()
+    {
+        var catalog = await ProjectService.CatalogProjectsAsync();
+
+        var result = catalog.CatalogProjects
+            .Where((n => PgContext.ArchivedProjects.Any(t => t.ProjectId == n.ProjectId)))
+            .ToList();
+
+        Assert.IsTrue(result.Count == 0);
+    }
+}
