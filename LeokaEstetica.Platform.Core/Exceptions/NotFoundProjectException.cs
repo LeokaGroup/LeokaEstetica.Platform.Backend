66c37f6eabd1fee53b96606c7395a88484d12d78
BlackSoulHub <mr.asaglaev@mail.ru>
2023-04-23 19:08:28 +0500

    Создан новый класс исключений

---
+++ LeokaEstetica.Platform.Core/Exceptions/NotFoundProjectException.cs
@@ -0,0 +1,9 @@
+namespace LeokaEstetica.Platform.Core.Exceptions;
+
+public class NotFoundProjectException : Exception
+{
+    public NotFoundProjectException(string message) : base(message)
+    {
+
+    }
+}
