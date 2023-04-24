86a9fe0a1ae7f83a5144ce92dc434461add8be13
BlackSoulHub <mr.asaglaev@mail.ru>
2023-04-24 18:00:42 +0500

    Добавлены тесты

---
+++ LeokaEstetica.Platform.Tests/ModerationTests/ApproveVacancyTest.cs
@@ -0,0 +1,32 @@
+using LeokaEstetica.Platform.Core.Exceptions;
+using LeokaEstetica.Platform.Models.Dto.Input.Vacancy;
+using NUnit.Framework;
+
+namespace LeokaEstetica.Platform.Tests.ModerationTests;
+
+[TestFixture]
+public class ApproveVacancyTest : BaseServiceTest
+{
+    /// <summary>
+    /// Успешный тест кейс
+    /// </summary>
+    [Test]
+    public async Task ApproveVacancyAsyncTest()
+    {
+        var result = await VacancyModerationService.ApproveVacancyAsync(299, 
+            null);
+        
+        Assert.IsTrue(result.IsSuccess);
+    }
+    
+    /// <summary>
+    /// Тест-кейс с ID несуществующей вакансии
+    /// </summary>
+    [Test]
+    public void ApproveVacancyAsyncThrowNotFoundExceptionTest()
+    {
+        Assert.ThrowsAsync<NotFoundProjectException>(async () =>
+            await VacancyModerationService.ApproveVacancyAsync(15, 
+                null));
+    }
+}
