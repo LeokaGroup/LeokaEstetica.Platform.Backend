a3a8a102b812fbe54e98753ea67b4c70f4fc086f
BlackSoulHub <mr.asaglaev@mail.ru>
2023-04-29 16:52:41 +0500

    [#10079018] Выравнивание документации

--- LeokaEstetica.Platform.Base/Abstractions/Services/Messaging/Mail/MailingsService.cs
+++ LeokaEstetica.Platform.Base/Abstractions/Services/Messaging/Mail/MailingsService.cs
@@ -18,13 +18,13 @@ public class MailingsService : IMailingsService
     }
 
    /// <summary>
-    /// Метод отправляет уведомление на почту о принятии инвайта в проект.
-    /// </summary>
-    /// <param name="mailTo">Почта пользователя, которому отправлять уведомление.</param>
-    /// <param name="projectId">Id проекта.</param>
-    /// <param name="projectName">Название проекта.</param>
-    /// <param name="vacancyName">Название вакансии.</param>
-    /// <param name="otherUser">Логин или почта пользователя, который оставил отклик.</param>
+   /// Метод отправляет уведомление на почту о принятии инвайта в проект.
+   /// </summary>
+   /// <param name="mailTo">Почта пользователя, которому отправлять уведомление.</param>
+   /// <param name="projectId">Id проекта.</param>
+   /// <param name="projectName">Название проекта.</param>
+   /// <param name="vacancyName">Название вакансии.</param>
+   /// <param name="otherUser">Логин или почта пользователя, который оставил отклик.</param>
    /// <param name="isEmailNotificationsDisableModeEnabled">Признак уведомлений на почту.</param>
     public async Task SendNotificationApproveInviteProjectAsync(string mailTo, long projectId, string projectName,
         string vacancyName, string otherUser, bool isEmailNotificationsDisableModeEnabled)
