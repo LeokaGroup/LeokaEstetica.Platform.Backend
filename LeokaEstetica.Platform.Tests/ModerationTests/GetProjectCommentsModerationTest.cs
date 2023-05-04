d60fd333a34898d54de14e62b3a4b4c95e66d31e
Lev <leva.100s@gmail.com>
2023-05-04 19:43:41 +0300

    #10165972 Добавлен тест.

---
+++ LeokaEstetica.Platform.Tests/ModerationTests/GetProjectCommentsModerationTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace LeokaEstetica.Platform.Tests.ModerationTests;
+
+[TestFixture]
+public class GetProjectCommentsModerationTest : BaseServiceTest
+{
+    [Test]
+    public async Task GetProjectModerationByProjectIdAsyncTest()
+    {
+        var result = await ProjectModerationService.GetProjectCommentsModerationAsync(215);
+
+        Assert.IsNotNull(result);
+    }
+}
