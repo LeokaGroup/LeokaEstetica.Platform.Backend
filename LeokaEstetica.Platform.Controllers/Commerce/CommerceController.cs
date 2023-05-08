39ab766f94f5e084ae517f7c54e401d2948e0467
Timoshenko A.I <sierra_93@mail.ru>
2023-05-08 17:33:05 +0300

    #10430075 Добавил ендпоинт для получения информации о тарифе для ФЗ.

--- LeokaEstetica.Platform.Controllers/Commerce/CommerceController.cs
+++ LeokaEstetica.Platform.Controllers/Commerce/CommerceController.cs
@@ -1,8 +1,11 @@
+using AutoMapper;
 using LeokaEstetica.Platform.Base;
 using LeokaEstetica.Platform.Controllers.Filters;
 using LeokaEstetica.Platform.Controllers.Validators.Commerce;
+using LeokaEstetica.Platform.Database.Abstractions.FareRule;
 using LeokaEstetica.Platform.Models.Dto.Input.Commerce.PayMaster;
 using LeokaEstetica.Platform.Models.Dto.Output.Commerce.PayMaster;
+using LeokaEstetica.Platform.Models.Dto.Output.FareRule;
 using LeokaEstetica.Platform.Processing.Abstractions.PayMaster;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,14 +20,22 @@ namespace LeokaEstetica.Platform.Controllers.Commerce;
 public class CommerceController : BaseController
 {
     private readonly IPayMasterService _payMasterService;
-    
+    private readonly IFareRuleRepository _fareRuleRepository;
+    private readonly IMapper _mapper;
+
     /// <summary>
     /// Конструктор.
     /// </summary>
     /// <param name="payMasterService">Сервис платежей через ПС PayMaster.</param>
-    public CommerceController(IPayMasterService payMasterService)
+    /// <param name="fareRuleRepository">Репозиторий правил тарифов.</param>
+    /// <param name="mapper">Автомаппер.</param>
+    public CommerceController(IPayMasterService payMasterService, 
+        IFareRuleRepository fareRuleRepository, 
+        IMapper mapper)
     {
         _payMasterService = payMasterService;
+        _fareRuleRepository = fareRuleRepository;
+        _mapper = mapper;
     }
 
     /// <summary>
@@ -52,6 +63,26 @@ public class CommerceController : BaseController
         }
         
         result = await _payMasterService.CreateOrderAsync(createOrderInput, GetUserName(), GetTokenFromHeader());
+
+        return result;
+    }
+
+    /// <summary>
+    /// Метод получает для ФЗ информацию о тарифе.
+    /// </summary>
+    /// <param name="publicId">Публичный ключ тарифа.</param>
+    /// <returns>Информация о тарифе.</returns>
+    [HttpGet]
+    [Route("fare-rule/order-form/{publicId}/info")]
+    [ProducesResponseType(200, Type = typeof(FareRuleOutput))]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(403)]
+    [ProducesResponseType(500)]
+    [ProducesResponseType(404)]
+    public async Task<FareRuleOutput> GetFareRuleInfoAsync([FromRoute] Guid publicId)
+    {
+        var fareRule = await _fareRuleRepository.GetByPublicIdAsync(publicId);
+        var result = _mapper.Map<FareRuleOutput>(fareRule);
 
         return result;
     }
