04e86abaa787217a433092f142caf1747d05b772
Timoshenko A.I <sierra_93@mail.ru>
2023-05-13 02:03:20 +0300

    #10460368 Добавил сервис коммерции.

---
+++ LeokaEstetica.Platform.Processing/Services/Commerce/CommerceService.cs
@@ -0,0 +1,13 @@
+using LeokaEstetica.Platform.Processing.Abstractions.Commerce;
+
+namespace LeokaEstetica.Platform.Processing.Services.Commerce;
+
+/// <summary>
+/// Класс реализует методы сервиса коммерции.
+/// </summary>
+public class CommerceService : ICommerceService
+{
+    public CommerceService()
+    {
+    }
+}
