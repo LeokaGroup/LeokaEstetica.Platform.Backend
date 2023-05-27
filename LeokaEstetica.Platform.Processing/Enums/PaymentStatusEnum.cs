39e5eef596301f8832bbe153b7ac36fad1952c6f
Timoshenko A.I <sierra_93@mail.ru>
2023-05-27 13:52:52 +0300

    #10752539 Добавил маппинги новых статусов из ПС. Их не было заведено в системе у нас.

--- LeokaEstetica.Platform.Processing/Enums/PaymentStatusEnum.cs
+++ LeokaEstetica.Platform.Processing/Enums/PaymentStatusEnum.cs
@@ -11,7 +11,22 @@ public enum PaymentStatusEnum
     None = 0,
     
     [Description("Новый")]
-    Pending = 1
+    Pending = 1,
+    
+    [Description("Платеж проведен")]
+    Settled = 2,
+    
+    [Description("Платеж авторизован")]
+    Authorized = 3,
+    
+    [Description("Платеж отменен")]
+    Cancelled = 4,
+    
+    [Description("Платеж отклонен")]
+    Rejected = 5,
+    
+    [Description("Требуется дополнительное подтверждение")]
+    Confirmation = 6
 }
 
 /// <summary>
@@ -21,9 +36,19 @@ public static class PaymentStatus
 {
     private static readonly Dictionary<string, PaymentStatusEnum> _paymentStatuses = new()
     {
-        { PaymentStatusEnum.Pending.ToString(), PaymentStatusEnum.Pending }
+        { PaymentStatusEnum.Pending.ToString(), PaymentStatusEnum.Pending },
+        { PaymentStatusEnum.Settled.ToString(), PaymentStatusEnum.Settled },
+        { PaymentStatusEnum.Authorized.ToString(), PaymentStatusEnum.Authorized },
+        { PaymentStatusEnum.Cancelled.ToString(), PaymentStatusEnum.Cancelled },
+        { PaymentStatusEnum.Rejected.ToString(), PaymentStatusEnum.Rejected },
+        { PaymentStatusEnum.Confirmation.ToString(), PaymentStatusEnum.Confirmation }
     };
 
+    /// <summary>
+    /// Метод првоеряет, есть ли такой статус в системе.
+    /// </summary>
+    /// <param name="statusName">Системное название статуса из ПС.</param>
+    /// <returns>Если статус есть, вернет его, иначе будет по дефолту None.</returns>
     public static PaymentStatusEnum GetPaymentStatusBySysName(string statusName)
     {
         return _paymentStatuses.TryGet(statusName);
