1e5373f87d161d050c6744507322f523a12474ce
Timoshenko A.I <sierra_93@mail.ru>
2023-06-03 01:46:43 +0300

    #10078791 Наполнил выходную модель.

--- LeokaEstetica.Platform.Models/Dto/Output/Orders/HistoryOutput.cs
+++ LeokaEstetica.Platform.Models/Dto/Output/Orders/HistoryOutput.cs
@@ -5,5 +5,38 @@ namespace LeokaEstetica.Platform.Models.Dto.Output.Orders;
 /// </summary>
 public class HistoryOutput
 {
-    
+    /// <summary>
+    /// PK.
+    /// </summary>
+    public long ShadowId { get; set; }
+
+    /// <summary>
+    /// Дата транзакции.
+    /// </summary>
+    public DateTime DateCreated { get; set; }
+
+    /// <summary>
+    /// Текст события.
+    /// </summary>
+    public string ActionText { get; set; }
+
+    /// <summary>
+    /// Системное название события.
+    /// </summary>
+    public string ActionSysName { get; set; }
+
+    /// <summary>
+    /// Id пользователя.
+    /// </summary>
+    public long UserId { get; set; }
+
+    /// <summary>
+    /// Id заказа.
+    /// </summary>
+    public long OrderId { get; set; }
+
+    /// <summary>
+    /// Название статуса.
+    /// </summary>
+    public string StatusName { get; set; }
 }
