476dde827a1c16082bb14131642506022c5b7f83
Timoshenko A.I <sierra_93@mail.ru>
2023-09-04 18:53:30 +0300

    #14050741 Фикс ошибок валидации на англ v.1.4

--- LeokaEstetica.Platform.Controllers/ModelsValidation/CustomLanguageManager.cs
+++ LeokaEstetica.Platform.Controllers/ModelsValidation/CustomLanguageManager.cs
@@ -7,7 +7,9 @@ public class CustomLanguageManager : FluentValidation.Resources.LanguageManager
 {
     public CustomLanguageManager() 
     {
-        AddTranslation("ru", "NotNullValidator", $"'{nameof(UserSignInInput.Email)}' {ValidationConsts.NOT_VALID_EMAIL_ERROR}");
-        AddTranslation("ru", "NotNullValidator", $"'{nameof(UserSignInInput.Password)}' {ValidationConsts.NOT_VALID_PASSWORD}");
+        // AddTranslation("ru", "NotNullValidator", $"'{nameof(UserSignInInput.Email)}' {ValidationConsts.NOT_VALID_EMAIL_ERROR}");
+        // AddTranslation("ru", "NotNullValidator", $"'{nameof(UserSignInInput.Password)}' {ValidationConsts.NOT_VALID_PASSWORD}");
+        AddTranslation("ru", "NotNullValidator", $"{nameof(UserSignInInput.Email)} {ValidationConsts.NOT_VALID_EMAIL_ERROR}");
+        AddTranslation("ru", "NotNullValidator", $"{nameof(UserSignInInput.Password)} {ValidationConsts.NOT_VALID_PASSWORD}");
     }
 }
