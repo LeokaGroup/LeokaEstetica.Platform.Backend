675b1e85afe8f52ce473b1847f62ccd6f0c7e8de
Timoshenko A.I <sierra_93@mail.ru>
2023-10-12 23:51:48 +0300

    Фикс ошибки сервиса на серве (ошибка с зависимостями) v.1.6.

--- LeokaEstetica.Platform.Backend/Loaders/Jobs/StartJobs.cs
+++ LeokaEstetica.Platform.Backend/Loaders/Jobs/StartJobs.cs
@@ -1,41 +1,41 @@
-using LeokaEstetica.Platform.Backend.Loaders.Jobs.RabbitMq;
-using LeokaEstetica.Platform.Backend.Loaders.Jobs.User;
-using Quartz;
-
-namespace LeokaEstetica.Platform.Backend.Loaders.Jobs;
-
-/// <summary>
-/// Класс с джобами, которые должны выполняться при запуске ядра системы.
-/// </summary>
-public static class StartJobs
-{
-    /// <summary>
-    /// Метод запускает все джобы.
-    /// </summary>
-    public static void Start(IServiceCollectionQuartzConfigurator q, IServiceCollection services)
-    {
-        // Запускаем планировщик активностей аккаунтов пользователей.
-        services.AddHostedService<UserActivityMarkDeactivateJob>();
-
-        // Запускаем планировщик удаления аккаунтов пользователей.
-        services.AddHostedService<DeleteDeactivatedAccountsJob>();
-
-        var refundsJobJobKey = new JobKey("RefundsJob");
-        q.AddJob<RefundsJob>(opts => opts.WithIdentity(refundsJobJobKey));
-        q.AddTrigger(opts => opts
-            .ForJob(refundsJobJobKey)
-            .WithIdentity("RefundsJobTrigger")
-            .WithSimpleSchedule(x => x
-                .WithIntervalInMinutes(3)
-                .RepeatForever()));
-        
-        var ordersJobJobKey = new JobKey("OrdersJob");
-        q.AddJob<OrdersJob>(opts => opts.WithIdentity(ordersJobJobKey));
-        q.AddTrigger(opts => opts
-            .ForJob(ordersJobJobKey)
-            .WithIdentity("OrdersJobTrigger")
-            .WithSimpleSchedule(x => x
-                .WithIntervalInMinutes(3)
-                .RepeatForever()));
-    }
-}
+// using LeokaEstetica.Platform.WorkerServices.Jobs.RabbitMq;
+// using LeokaEstetica.Platform.WorkerServices.Jobs.User;
+// using Quartz;
+//
+// namespace LeokaEstetica.Platform.Backend.Loaders.Jobs;
+//
+// /// <summary>
+// /// Класс с джобами, которые должны выполняться при запуске ядра системы.
+// /// </summary>
+// public static class StartJobs
+// {
+//     /// <summary>
+//     /// Метод запускает все джобы.
+//     /// </summary>
+//     public static void Start(IServiceCollectionQuartzConfigurator q, IServiceCollection services)
+//     {
+//         // Запускаем планировщик активностей аккаунтов пользователей.
+//         services.AddHostedService<UserActivityMarkDeactivateJob>();
+//
+//         // Запускаем планировщик удаления аккаунтов пользователей.
+//         services.AddHostedService<DeleteDeactivatedAccountsJob>();
+//
+//         var refundsJobJobKey = new JobKey("RefundsJob");
+//         q.AddJob<RefundsJob>(opts => opts.WithIdentity(refundsJobJobKey));
+//         q.AddTrigger(opts => opts
+//             .ForJob(refundsJobJobKey)
+//             .WithIdentity("RefundsJobTrigger")
+//             .WithSimpleSchedule(x => x
+//                 .WithIntervalInMinutes(3)
+//                 .RepeatForever()));
+//         
+//         var ordersJobJobKey = new JobKey("OrdersJob");
+//         q.AddJob<OrdersJob>(opts => opts.WithIdentity(ordersJobJobKey));
+//         q.AddTrigger(opts => opts
+//             .ForJob(ordersJobJobKey)
+//             .WithIdentity("OrdersJobTrigger")
+//             .WithSimpleSchedule(x => x
+//                 .WithIntervalInMinutes(3)
+//                 .RepeatForever()));
+//     }
+// }
