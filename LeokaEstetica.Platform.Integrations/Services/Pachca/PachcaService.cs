f0a1174ebcf8c32f2c85cabf06645e7a7ef1986b
Timoshenko A.I <sierra_93@mail.ru>
2023-12-07 13:05:48 +0300

    Фикс уведомлений в пачку.
    (cherry picked from commit 53c8fd292609156b8628832c0e61b10aba1b3830)

--- LeokaEstetica.Platform.Integrations/Services/Pachca/PachcaService.cs
+++ LeokaEstetica.Platform.Integrations/Services/Pachca/PachcaService.cs
@@ -153,8 +153,11 @@ internal sealed class PachcaService : IPachcaService
     /// <param name="account">Аккаунт пользователя.</param>
     public async Task SendNotificationCreatedNewUserAsync(string account)
     {
-        var notifyMessage = $"Новый пользователь {account} на платформе.";
         using var httpClient = new HttpClient();
+        var notificationInput = new SendNotificationInput
+        {
+            Message = $"Новый пользователь {account} на платформе!"
+        };
 
         try
         {
@@ -168,12 +171,12 @@ internal sealed class PachcaService : IPachcaService
             if (new[] { "Development", "Staging" }.Contains(_configuration["Environment"]))
             {
                 await httpClient.PostAsJsonAsync(_configuration["PachcaBot:NotificationsDevelopTestBot"],
-                    notifyMessage);
+                    notificationInput);
             }
 
             else
             {
-                await httpClient.PostAsJsonAsync(_configuration["PachcaBot:NotificationsBot"], notifyMessage);
+                await httpClient.PostAsJsonAsync(_configuration["PachcaBot:NotificationsBot"], notificationInput);
             }
         }
 
@@ -188,12 +191,12 @@ internal sealed class PachcaService : IPachcaService
                 if (new[] { "Development", "Staging" }.Contains(_configuration["Environment"]))
                 {
                     await httpClient.PostAsJsonAsync(_configuration["PachcaBot:NotificationsDevelopTestBot"],
-                        notifyMessage);
+                        notificationInput);
                 }
 
                 else
                 {
-                    await httpClient.PostAsJsonAsync(_configuration["PachcaBot:NotificationsBot"], notifyMessage);
+                    await httpClient.PostAsJsonAsync(_configuration["PachcaBot:NotificationsBot"], notificationInput);
                 }
             }
 
