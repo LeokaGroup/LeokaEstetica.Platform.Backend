18c06c282c3868471677502aabc5c16cb7b37c83
Timoshenko A.I <sierra_93@mail.ru>
2023-12-09 13:11:05 +0300

    #26409494 Добавил сущносить, маппинг и таблицу для приоритетов.

---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/041.Esta.DDL_Create_TaskPriorities.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "ProjectManagment"."TaskPriorities"
+(
+    "PriorityId"      INT,
+    "PriorityName"    VARCHAR(50) NOT NULL,
+    "PrioritySysName" VARCHAR(50) NOT NULL,
+    "Position"        INT         NOT NULL DEFAULT 0,
+    CONSTRAINT "PK_TaskPriorities_PriorityId" PRIMARY KEY ("PriorityId")
+);
