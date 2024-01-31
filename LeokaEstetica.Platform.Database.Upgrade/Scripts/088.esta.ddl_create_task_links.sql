a00d23f307814829010b187d577dbaafa3d41452
Timoshenko A.I <sierra_93@mail.ru>
2024-01-31 23:06:29 +0300

    #28174863 Добавил миграции для создания таблицы связей между задачами и енамки типов связей.

---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/088.esta.ddl_create_task_links.sql
@@ -0,0 +1,19 @@
+CREATE TABLE project_management.task_links
+(
+    link_id   BIGSERIAL,
+    task_id   BIGINT NOT NULL,
+    link_type project_management.link_type_enum,
+    parent_id BIGINT NULL,
+    child_id  BIGINT NULL,
+    CONSTRAINT pk_task_links_link_id PRIMARY KEY (link_id),
+    CONSTRAINT fk_project_tasks_project_task_id FOREIGN KEY (task_id) REFERENCES project_management.project_tasks (task_id),
+    CONSTRAINT fk_project_tasks_parent_id FOREIGN KEY (parent_id) REFERENCES project_management.project_tasks (task_id),
+    CONSTRAINT fk_project_tasks_child_id FOREIGN KEY (child_id) REFERENCES project_management.project_tasks (task_id)
+);
+
+COMMENT ON TABLE project_management.task_links IS 'Таблица связей между задачами.';
+COMMENT ON COLUMN project_management.task_links.link_id IS 'PK.';
+COMMENT ON COLUMN project_management.task_links.task_id IS 'Id задачи.';
+COMMENT ON COLUMN project_management.task_links.link_type IS 'Тип связи.';
+COMMENT ON COLUMN project_management.task_links.parent_id IS 'Id родителя.';
+COMMENT ON COLUMN project_management.task_links.child_id IS 'Id дочки.';
