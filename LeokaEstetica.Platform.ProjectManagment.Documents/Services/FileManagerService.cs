22eacbe867c21420fc9e0a75009e68f3b1d11f30
Timoshenko A.I <sierra_93@mail.ru>
2024-02-15 16:11:04 +0300

    #28969068 Минорный рефач.

--- LeokaEstetica.Platform.ProjectManagment.Documents/Services/FileManagerService.cs
+++ LeokaEstetica.Platform.ProjectManagment.Documents/Services/FileManagerService.cs
@@ -229,29 +229,4 @@ internal sealed class FileManagerService : IFileManagerService
                 
         return ms.ToArray();
     }
-    
-    // public byte[] UseStreamReader(Stream stream)
-    // {
-    //     byte[] bytes;
-    //     using (var reader = new StreamReader(stream))
-    //     {
-    //         bytes = System.Text.Encoding.UTF8.GetBytes(reader.ReadToEnd());
-    //     }
-    //     return bytes;
-    // }
-    
-    // public byte[] UseMemoryStream(Stream stream)
-    // {
-    //     byte[] bytes;
-    //     if (stream is MemoryStream memStream)
-    //     {
-    //         return memStream.ToArray();
-    //     }
-    //     using (var memoryStream = new MemoryStream())
-    //     {
-    //         stream.CopyTo(memoryStream);
-    //         bytes = memoryStream.ToArray();
-    //     }
-    //     return bytes;
-    // }
 }
