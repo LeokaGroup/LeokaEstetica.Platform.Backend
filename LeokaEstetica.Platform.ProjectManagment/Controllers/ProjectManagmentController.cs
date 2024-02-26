7d7c3de0a31d433cc8f2dd4c0102b4831ade6b75
Timoshenko A.I <sierra_93@mail.ru>
2024-02-26 17:50:50 +0300

    #29690136 Реализовал изменение комментария + добавил тест на это.

--- LeokaEstetica.Platform.ProjectManagment/Controllers/ProjectManagmentController.cs
+++ LeokaEstetica.Platform.ProjectManagment/Controllers/ProjectManagmentController.cs
@@ -1450,4 +1450,44 @@ public class ProjectManagmentController : BaseController
 
         return result;
     }
+
+    /// <summary>
+    /// Метод обновляет комментарий задачи.
+    /// </summary>
+    /// <param name="taskCommentInput">Входная модель.</param>
+    [HttpPut]
+    [Route("task-comment")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(403)]
+    [ProducesResponseType(500)]
+    [ProducesResponseType(404)]
+    public async Task UpdateTaskCommentAsync([FromBody] TaskCommentExtendedInput taskCommentInput)
+    {
+        var validator = await new TaskCommentExtendedValidator().ValidateAsync(taskCommentInput);
+
+        if (validator.Errors.Any())
+        {
+            var exceptions = new List<InvalidOperationException>();
+
+            foreach (var err in validator.Errors)
+            {
+                exceptions.Add(new InvalidOperationException(err.ErrorMessage));
+            }
+
+            var ex = new AggregateException("Ошибка изменения комментария задачи. " +
+                                            $"ProjectId: {taskCommentInput.ProjectId}. " +
+                                            $"ProjectTaskId: {taskCommentInput.ProjectTaskId}. " +
+                                            $"Comment: {taskCommentInput.Comment}." +
+                                            $"CommentId: {taskCommentInput.CommentId}", exceptions);
+            _logger.LogError(ex, ex.Message);
+            
+            await _pachcaService.Value.SendNotificationErrorAsync(ex);
+            
+            throw ex;
+        }
+
+        await _projectManagmentService.UpdateTaskCommentAsync(taskCommentInput.ProjectTaskId,
+            taskCommentInput.ProjectId, taskCommentInput.CommentId, taskCommentInput.Comment, GetUserName());
+    }
 }
