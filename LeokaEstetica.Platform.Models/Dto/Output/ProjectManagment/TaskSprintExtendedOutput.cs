b6bc853a7a33e488376bf09b4a9b7e37c366779b
Timoshenko A.I <sierra_93@mail.ru>
2024-05-02 23:17:29 +0300

    #30514055 Доработка модели спринта.

--- LeokaEstetica.Platform.Models/Dto/Output/ProjectManagment/TaskSprintExtendedOutput.cs
+++ LeokaEstetica.Platform.Models/Dto/Output/ProjectManagment/TaskSprintExtendedOutput.cs
@@ -40,11 +40,3 @@ public class TaskSprintExtendedOutput : TaskSprintOutput
     /// </summary>
     public long ProjectSprintId { get; set; }
 }
-
-/// <summary>
-/// Класс выходной модели задачи спринта.
-/// </summary>
-public class SprintTaskOutput
-{
-    
-}
