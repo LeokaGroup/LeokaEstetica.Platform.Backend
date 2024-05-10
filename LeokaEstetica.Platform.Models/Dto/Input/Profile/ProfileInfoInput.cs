5b2ea2d9c3b975171f343bc55ad6a14dc9321a03
Timoshenko A.I <sierra_93@mail.ru>
2024-05-10 13:59:05 +0300

    Фикс ошибок валидации анкеты.

--- LeokaEstetica.Platform.Models/Dto/Input/Profile/ProfileInfoInput.cs
+++ LeokaEstetica.Platform.Models/Dto/Input/Profile/ProfileInfoInput.cs
@@ -8,6 +8,33 @@ namespace LeokaEstetica.Platform.Models.Dto.Input.Profile;
 public class ProfileInfoInput
 {
     /// <summary>
+    /// Конструктор.
+    /// </summary>
+    /// <param name="lastName">Фамилия.</param>
+    /// <param name="firstName">Имя.</param>
+    /// <param name="aboutme">Обо мне.</param>
+    /// <param name="job">Должность.</param>
+    /// <param name="email">Почта пользователя.</param>
+    /// <param name="phoneNumber">Номер телефона.</param>
+    /// <param name="userSkills">Список навыков пользователя, которые он выбрал.</param>
+    /// <param name="userIntents">Список целей пользователя, которые он выбрал.</param>
+    /// <param name="workExperience">Опыт работы.</param>
+    public ProfileInfoInput(string lastName, string firstName, string aboutme, string job, string email,
+        string phoneNumber, IEnumerable<SkillInput> userSkills, IEnumerable<IntentOutput> userIntents,
+        string workExperience)
+    {
+        LastName = lastName;
+        FirstName = firstName;
+        Aboutme = aboutme;
+        Job = job;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        UserSkills = userSkills;
+        UserIntents = userIntents;
+        WorkExperience = workExperience;
+    }
+
+    /// <summary>
     /// Фамилия.
     /// </summary>
     public string LastName { get; set; }
@@ -20,7 +47,7 @@ public class ProfileInfoInput
     /// <summary>
     /// Отчество.
     /// </summary>
-    public string Patronymic { get; set; }
+    public string? Patronymic { get; set; }
 
     /// <summary>
     /// Отображать ли первую букву фамилии.
@@ -30,22 +57,22 @@ public class ProfileInfoInput
     /// <summary>
     /// Ссылка на телегу либо ник.
     /// </summary>
-    public string Telegram { get; set; }
+    public string? Telegram { get; set; }
 
     /// <summary>
     /// Ватсап номер телефона.
     /// </summary>
-    public string WhatsApp { get; set; }
+    public string? WhatsApp { get; set; }
 
     /// <summary>
     /// Ссылка на ВК либо ник.
     /// </summary>
-    public string Vkontakte { get; set; }
+    public string? Vkontakte { get; set; }
 
     /// <summary>
     /// Ссылка на другую соц.сеть.
     /// </summary>
-    public string OtherLink { get; set; }
+    public string? OtherLink { get; set; }
 
     /// <summary>
     /// Обо мне.
