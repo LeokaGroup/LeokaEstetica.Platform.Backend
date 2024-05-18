794a5945d3f46eee095fd2ea74cea87496e90e99
Timoshenko A.I <sierra_93@mail.ru>
2024-05-18 22:26:28 +0300

    #33603997 Добавил валидацию к обучению из CSV.

--- LeokaEstetica.Platform.ProjectManagement/Controllers/ScrumMasterAiController.cs
+++ LeokaEstetica.Platform.ProjectManagement/Controllers/ScrumMasterAiController.cs
@@ -1,12 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LeokaEstetica.Platform.Base;
+using LeokaEstetica.Platform.Base.Filters;
+using LeokaEstetica.Platform.Integrations.Abstractions.Discord;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LeokaEstetica.Platform.ProjectManagement.Controllers;
 
-public class ScrumMasterAiController : Controller
+/// <summary>
+/// Контроллер нейросети ScrumMasterAi.
+/// </summary>
+[ApiController]
+[Route("project-management/scrum-master-ai")]
+[AuthFilter]
+public class ScrumMasterAiController : BaseController
 {
-    // GET
-    public IActionResult Index()
+    private readonly ILogger<ScrumMasterAiController> _logger;
+    private readonly Lazy<IDiscordService> _discordService;
+    
+    /// <summary>
+    /// Конструктор.
+    /// <param name="logger">Логгер.</param>
+    /// <param name="discordService">Сервис уведомлений дискорда.</param>
+    /// </summary>
+    public ScrumMasterAiController(ILogger<ScrumMasterAiController> logger,
+     Lazy<IDiscordService> discordService)
     {
-        return View();
+        _logger = logger;
+        _discordService = discordService;
+    }
+
+    /// <summary>
+    /// TODO: Добавить ограничение на роль пользователя. Только некоторым можно учить ее, проверять на роль.
+    /// Метод обучает нейросеть из датасета в формате csv.
+    /// </summary>
+    /// <param name="file">Датасет в csv.</param>
+    [HttpPost]
+    [Route("education")]
+    public async Task EducationFromCsvDatasetAsync([FromForm] IFormFile file)
+    {
+        if (file.Length == 0)
+        {
+            var ex = new InvalidOperationException(
+                "Не передан файл для обучения нейросети. Требуемый формат файла: .CSV");
+            _logger.LogError(ex ,ex.Message);
+
+            await _discordService.Value.SendNotificationErrorAsync(ex);
+        }
     }
 }
