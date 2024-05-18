997767ccdd29f00965c7c30d803181fcf0b39a47
Timoshenko A.I <sierra_93@mail.ru>
2024-05-18 21:11:20 +0300

    #33603997 Начал добавлять ендпоинт и датасеты для обучения нейросети.

---
+++ LeokaEstetica.Platform.ProjectManagement/Controllers/ScrumMasterAiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeokaEstetica.Platform.ProjectManagement.Controllers;
+
+public class ScrumMasterAiController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
