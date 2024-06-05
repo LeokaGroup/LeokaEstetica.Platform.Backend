ca00301e1f70180141fc628a70e72b42b529bed5
Timoshenko A.I <sierra_93@mail.ru>
2024-06-05 23:08:54 +0300

    #34545024 Докинул миграции.

---
+++ LeokaEstetica.Platform.ProjectManagement/Controllers/ProjectManagementRoleController.cs
@@ -0,0 +1,16 @@
+﻿using LeokaEstetica.Platform.Base;
+using LeokaEstetica.Platform.Base.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeokaEstetica.Platform.ProjectManagement.Controllers;
+
+/// <summary>
+/// Контроллер ролей управления проектами.
+/// </summary>
+[ApiController]
+[Route("project-management-role")]
+[AuthFilter]
+public class ProjectManagementRoleController : BaseController
+{
+    
+}
