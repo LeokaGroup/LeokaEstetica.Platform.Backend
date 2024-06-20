82b0fa6aed15ae42f280bb38bdfec066d7f48f11
Sultan Galiullin <sultangal@outlook.com>
2024-06-20 17:33:08 +0300

    #31632148. Добавил скрипт, удаляющий дубликат object_tag_type_enum из схемы public.

---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/256.esta.ddl_delete_object_tag_type_enum_duplication.sql
@@ -0,0 +1,7 @@
+﻿ALTER TABLE project_management.project_tags
+    DROP COLUMN object_tag_type;
+
+ALTER TABLE project_management.project_tags
+    ADD COLUMN object_tag_type project_management.object_tag_type_enum;
+
+DROP TYPE public.object_tag_type_enum;
