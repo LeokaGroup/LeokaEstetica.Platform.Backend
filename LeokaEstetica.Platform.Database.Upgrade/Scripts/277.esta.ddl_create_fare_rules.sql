0715a2fd3041d0c8cc1e6a3a7d79d339967926f7
Timoshenko A.I <sierra_93@mail.ru>
2024-07-12 03:07:42 +0300

    #35846829 Изменения в миграциях.

--- LeokaEstetica.Platform.Database.Upgrade/Scripts/277.esta.ddl_create_fare_rules.sql
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/277.esta.ddl_create_fare_rules.sql
@@ -2,19 +2,13 @@
 (
     rule_id             SERIAL         NOT NULL,
     rule_name           VARCHAR(150)   NOT NULL,
-    employees_count     INT            NOT NULL DEFAULT 1,
-    feature             VARCHAR(150)   NOT NULL,
-    price               NUMERIC(12, 2) NULL,
-    post_vacancy        VARCHAR(100)   NOT NULL,
-    access_user_profile VARCHAR(100)   NOT NULL,
+    position INT NOT NULL DEFAULT 0,
+    is_free      BOOLEAN NOT NULL DEFAULT FALSE,
     CONSTRAINT pk_rule_id PRIMARY KEY (rule_id)
 );
 
 COMMENT ON TABLE rules.fare_rules IS 'Таблица тарифов.';
 COMMENT ON COLUMN rules.fare_rules.rule_id IS 'PK.';
 COMMENT ON COLUMN rules.fare_rules.rule_name IS 'Название тарифа.';
-COMMENT ON COLUMN rules.fare_rules.employees_count IS 'Кол-во сотрудников.';
-COMMENT ON COLUMN rules.fare_rules.feature IS 'Функционал.';
-COMMENT ON COLUMN rules.fare_rules.price IS 'Цена тарифа.';
-COMMENT ON COLUMN rules.fare_rules.post_vacancy IS 'Размещение  вакансий.';
-COMMENT ON COLUMN rules.fare_rules.access_user_profile IS 'Доступ к базе резюме.';
+COMMENT ON COLUMN rules.fare_rules.position IS 'Позиция.';
+COMMENT ON COLUMN rules.fare_rules.is_free IS 'Признак бесплатного тарифа.';
