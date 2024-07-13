1fe8c6313e11429dae5a1740c4f316a1988df252
Timoshenko A.I <sierra_93@mail.ru>
2024-07-13 13:35:34 +0300

    #35846829 Фикс скриптов.

--- LeokaEstetica.Platform.Database.Upgrade/Scripts/280.esta.dml_insert_fare_rules.sql
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/280.esta.dml_insert_fare_rules.sql
@@ -1,5 +1,5 @@
-﻿INSERT INTO rules.fare_rules (rule_name, position, is_free)
-VALUES ('Бесплатный', 1, TRUE),
-       ('Стартовый', 2, FALSE),
-       ('Основной', 3, FALSE),
-       ('Комплексный', 4, FALSE);
+﻿INSERT INTO rules.fare_rules (rule_id, rule_name, position, is_free)
+VALUES  (1, 'Бесплатный', 1, true),
+        (2, 'Стартовый', 2, false),
+        (3, 'Основной', 3, false),
+        (4, 'Комплексный', 4, false);
