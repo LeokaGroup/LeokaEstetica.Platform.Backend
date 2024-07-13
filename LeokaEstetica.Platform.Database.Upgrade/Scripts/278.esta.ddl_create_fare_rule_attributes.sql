a0fab3575d07a259d3b6a7d9cd4a10124fe7a38d
Timoshenko A.I <sierra_93@mail.ru>
2024-07-13 03:34:00 +0300

    #35846829 Докинул миграции на изменение структуры таблиц тарифов и вставку данных.

--- LeokaEstetica.Platform.Database.Upgrade/Scripts/278.esta.ddl_create_fare_rule_attributes.sql
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/278.esta.ddl_create_fare_rule_attributes.sql
@@ -3,19 +3,11 @@
     attribute_id      SERIAL       NOT NULL,
     attribute_name    VARCHAR(150) NOT NULL,
     attribute_details TEXT         NOT NULL,
-    is_price          BOOLEAN      NOT NULL DEFAULT FALSE,
-    rule_id           INT          NOT NULL,
-    price NUMERIC (12, 2) NULL,
-    measure VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_fare_rule_attributes_attribute_id PRIMARY KEY (attribute_id),
-    CONSTRAINT fk_fare_rules_rule_id FOREIGN KEY (rule_id) REFERENCES rules.fare_rules (rule_id)
+    position          INT          NOT NULL DEFAULT 0,
+    CONSTRAINT pk_fare_rule_attributes_attribute_id PRIMARY KEY (attribute_id)
 );
 
 COMMENT ON TABLE rules.fare_rule_attributes IS 'Таблица элементов тарифов.';
 COMMENT ON COLUMN rules.fare_rule_attributes.attribute_id IS 'PK.';
 COMMENT ON COLUMN rules.fare_rule_attributes.attribute_name IS 'Название атрибута тарифа';
 COMMENT ON COLUMN rules.fare_rule_attributes.attribute_details IS 'Описание атрибутов тарифа.';
-COMMENT ON COLUMN rules.fare_rule_attributes.is_price IS 'Наличие цены у тарифа.';
-COMMENT ON COLUMN rules.fare_rule_attributes.rule_id IS 'Id тарифа.';
-COMMENT ON COLUMN rules.fare_rule_attributes.price IS 'Цена тарифа.';
-COMMENT ON COLUMN rules.fare_rule_attributes.measure IS 'Ед.изм. тарифа.';
---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/280.esta.dml_insert_fare_rules.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO rules.fare_rules (rule_name, position, is_free)
+VALUES ('Бесплатный', 1, TRUE),
+       ('Стартовый', 2, FALSE),
+       ('Основной', 3, FALSE),
+       ('Комплексный', 4, FALSE);
