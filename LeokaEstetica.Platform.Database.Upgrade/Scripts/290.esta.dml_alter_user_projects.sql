333faa2920a43ff8b97d62f90ea15705bc134f86
Sultan Galiullin <sultangal@outlook.com>
2024-07-15 18:13:36 +0300

    #25320917. Добавил колонку IsPublic в таблицу UserProjects. Накатил через миграции.

---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/290.esta.dml_alter_user_projects.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE IF EXISTS "Projects"."UserProjects"
+    ADD COLUMN IF NOT EXISTS "IsPublic" BOOL DEFAULT TRUE;
+COMMENT ON COLUMN "Projects"."UserProjects"."IsPublic" IS 'Поле видимости проекта.';
