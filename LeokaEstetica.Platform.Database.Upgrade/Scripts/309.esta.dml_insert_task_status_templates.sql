3dbe776c73bcd5450986294a2216288efc4dfe5d
Timoshenko A.I <sierra_93@mail.ru>
2024-08-24 15:26:15 +0300

    #36596353 Докинул миграции на добавление новых статусов эпика, истории и удалил дубль индекса в переходах.

---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/309.esta.dml_insert_task_status_templates.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO templates.project_management_task_status_templates (status_name, status_sys_name, "position",
+                                                                task_status_id, status_description, is_system_status)
+VALUES ('Новый', 'New', 10, 1, NULL, TRUE),
+       ('Завершен', 'Completed', 11, 1, NULL, TRUE),
+       ('Завершена', 'Completed', 12, 1, NULL, TRUE),
+       ('Закрыт', 'Closed', 13, 1, NULL, TRUE),
+       ('Закрыта', 'Closed', 14, 1, NULL, TRUE);
