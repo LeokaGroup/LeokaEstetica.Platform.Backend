b9083d5a16c099ee65671deec3516d284b23eb18
Timoshenko A.I <sierra_93@mail.ru>
2024-09-18 21:04:13 +0300

    #38928799 Добавил в контроллере логику.

--- LeokaEstetica.Platform.ProjectManagement/Controllers/CompanyController.cs
+++ LeokaEstetica.Platform.ProjectManagement/Controllers/CompanyController.cs
@@ -3,6 +3,7 @@ using LeokaEstetica.Platform.Base.Filters;
 using LeokaEstetica.Platform.Integrations.Abstractions.Discord;
 using LeokaEstetica.Platform.Models.Dto.Input.ProjectManagement;
 using LeokaEstetica.Platform.ProjectManagement.Validators;
+using LeokaEstetica.Platform.Services.Abstractions.ProjectManagment;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LeokaEstetica.Platform.ProjectManagement.Controllers;
@@ -17,17 +18,21 @@ public class CompanyController : BaseController
 {
     private readonly ILogger<CompanyController> _logger;
     private readonly Lazy<IDiscordService> _discordService;
+    private readonly ICompanyService _companyService;
     
     /// <summary>
     /// Конструктор.
     /// <param name="logger">Логгер.</param>
     /// <param name="discordService">Сервис уведомлений дискорда.</param>
+    /// <param name="companyService">Сервис компаний.</param>
     /// </summary>
     public CompanyController(ILogger<CompanyController> logger,
-        Lazy<IDiscordService> discordService)
+        Lazy<IDiscordService> discordService,
+        ICompanyService companyService)
     {
         _logger = logger;
         _discordService = discordService;
+        _companyService = companyService;
     }
 
     /// <summary>
@@ -60,5 +65,7 @@ public class CompanyController : BaseController
             
             throw ex;
         }
+
+        await _companyService.CreateCompanyAsync(companyInput.CompanyName, GetUserName());
     }
 }
