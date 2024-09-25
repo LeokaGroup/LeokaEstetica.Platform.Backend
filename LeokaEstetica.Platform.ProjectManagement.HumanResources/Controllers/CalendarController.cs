06b19270a491978220f472685ca2d3509b5ad1d1
Timoshenko A.I <sierra_93@mail.ru>
2024-09-25 22:31:21 +0300

    #39089615 Добавил отображение нужных данных в модалку создания события.

--- LeokaEstetica.Platform.ProjectManagement.HumanResources/Controllers/CalendarController.cs
+++ LeokaEstetica.Platform.ProjectManagement.HumanResources/Controllers/CalendarController.cs
@@ -1,6 +1,8 @@
 ﻿using LeokaEstetica.Platform.Base;
 using LeokaEstetica.Platform.Base.Filters;
+using LeokaEstetica.Platform.Core.Extensions;
 using LeokaEstetica.Platform.Models.Dto.Output.ProjectManagementHumanResources;
+using LeokaEstetica.Platform.Models.Enums;
 using LeokaEstetica.Platform.ProjectManagement.HumanResources.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 
@@ -41,5 +43,38 @@ public class CalendarController : BaseController
         var result = await _calendarService.GetCalendarEventsAsync(GetUserName());
 
         return result;
+    }
+
+    /// <summary>
+    /// Метод получает типы занятости.
+    /// </summary>
+    /// <returns>Список типов занятости.</returns>
+    [HttpGet]
+    [Route("busy-variants")]
+    [ProducesResponseType(200, Type = typeof(Dictionary<int, string>))]
+    [ProducesResponseType(400)]
+    [ProducesResponseType(403)]
+    [ProducesResponseType(500)]
+    [ProducesResponseType(404)]
+    public Task<List<(int Id, string Value)>> GetBusyVariantsAsync()
+    {
+        return Task.FromResult(new List<(int Id, string Value)>
+        {
+            new()
+            {
+                Id = (int)CalendarEventMemberStatusEnum.Busy,
+                Value = CalendarEventMemberStatusEnum.Busy.GetEnumDescription()
+            },
+            new()
+            {
+                Id = (int)CalendarEventMemberStatusEnum.MayBeBusy,
+                Value = CalendarEventMemberStatusEnum.MayBeBusy.GetEnumDescription()
+            },
+            new()
+            {
+                Id = (int)CalendarEventMemberStatusEnum.Available,
+                Value = CalendarEventMemberStatusEnum.Available.GetEnumDescription()
+            }
+        });
     }
 }
