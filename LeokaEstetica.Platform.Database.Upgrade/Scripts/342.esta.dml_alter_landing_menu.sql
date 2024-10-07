df95b874bd6ffe4cec8246b6fc6bc1416d517894
Timoshenko A.I <sierra_93@mail.ru>
2024-10-07 17:40:47 +0300

    #39689613 Завел миграции.

---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/342.esta.dml_alter_landing_menu.sql
@@ -0,0 +1,66 @@
+﻿INSERT INTO dbo.landing_menu (items)
+VALUES ('{
+    "items": [
+        {
+            "id": "FareRules",
+            "icon": null,
+            "items": [],
+            "label": null,
+            "command": null,
+            "tooltip": "Тарифы",
+            "visible": true,
+            "disabled": false,
+            "position": 1,
+            "tooltipPosition": null
+        },
+        {
+            "id": "Solutions",
+            "icon": null,
+            "items": [
+                {
+                    "id": "ProjectManagement",
+                    "icon": null,
+                    "items": [],
+                    "label": null,
+                    "command": null,
+                    "tooltip": "Модуль управления проектами",
+                    "visible": true,
+                    "disabled": false,
+                    "position": 1,
+                    "tooltipPosition": null
+                },
+                {
+                    "id": "HR",
+                    "icon": null,
+                    "items": [],
+                    "label": null,
+                    "command": null,
+                    "tooltip": "Модуль управления персоналом (HR)",
+                    "visible": true,
+                    "disabled": false,
+                    "position": 2,
+                    "tooltipPosition": null
+                }
+            ],
+            "label": null,
+            "command": null,
+            "tooltip": "Тарифы",
+            "visible": true,
+            "disabled": false,
+            "position": 2,
+            "tooltipPosition": null
+        },
+        {
+            "id": "Aboutme",
+            "icon": null,
+            "items": [],
+            "label": null,
+            "command": null,
+            "tooltip": "О нас",
+            "visible": true,
+            "disabled": false,
+            "position": 3,
+            "tooltipPosition": null
+        }
+    ]
+}');
