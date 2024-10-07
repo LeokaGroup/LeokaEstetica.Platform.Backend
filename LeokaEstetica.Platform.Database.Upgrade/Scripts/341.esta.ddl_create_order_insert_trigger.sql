5e78ebcb0d8767f86d0713fbbd9dec04432abef7
Hihiz <public@mail.ru>
2024-10-07 22:27:45 +0300

    #37799960 Добавить поле статуса в триггеры

--- LeokaEstetica.Platform.Database.Upgrade/Scripts/341.esta.ddl_create_order_insert_trigger.sql
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/341.esta.ddl_create_order_insert_trigger.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE FUNCTION commerce.order_transactions_history_insert()
 RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO commerce.order_transaction_history (created_at, action_text, action_sys_name, 
-													  created_by, order_id, order_type)
+													  created_by, order_id, order_type, status_name)
     VALUES (NEW.created_at, 'Создание статуса заказа.', 
-			'Insert', NEW.created_by, NEW.order_id, NEW.order_type);
+			'Insert', NEW.created_by, NEW.order_id, NEW.order_type, NEW.status_name);
     
     RETURN NEW;
 END;
--- LeokaEstetica.Platform.Database.Upgrade/Scripts/342.esta.ddl_create_order_update_trigger.sql
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/342.esta.ddl_create_order_update_trigger.sql
@@ -2,9 +2,9 @@ CREATE OR REPLACE FUNCTION commerce.order_transactions_history_update()
 RETURNS TRIGGER AS $$
 BEGIN
     INSERT INTO commerce.order_transaction_history (created_at, action_text, action_sys_name, 
-													  created_by, order_id, order_type)
+													  created_by, order_id, order_type, status_name)
     VALUES (OLD.created_at, 'Изменение статуса заказа.', 
-			'Update', OLD.created_by, OLD.order_id, OLD.order_type);
+			'Update', OLD.created_by, OLD.order_id, OLD.order_type, NEW.status_name);
     
     RETURN NEW;
 END;
