57638a645ccee334a756f5a691cdf80935622338
Hihiz <public@mail.ru>
2024-10-07 21:29:33 +0300

    #37799960 Добавить триггер на обновление

---
+++ LeokaEstetica.Platform.Database.Upgrade/Scripts/342.esta.ddl_create_order_update_trigger.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION commerce.order_transactions_history_update()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO commerce.order_transaction_history (created_at, action_text, action_sys_name, 
+													  created_by, order_id, order_type)
+    VALUES (OLD.created_at, 'Изменение статуса заказа.', 
+			'Update', OLD.created_by, OLD.order_id, OLD.order_type);
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER order_transactions_history_update_trigger
+	AFTER UPDATE 
+	ON commerce.orders
+	FOR EACH ROW
+	EXECUTE FUNCTION commerce.order_transactions_history_update()
