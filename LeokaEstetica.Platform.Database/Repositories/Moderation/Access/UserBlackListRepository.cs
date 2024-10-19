11b577c2e07a4a394c13b49859ff1a0038e06d2e
Hihiz <public@mail.ru>
2024-10-19 21:35:23 +0300

    #34440840 Изменить метод блокировки пользователя, добавить Dapper

--- LeokaEstetica.Platform.Database/Repositories/Moderation/Access/UserBlackListRepository.cs
+++ LeokaEstetica.Platform.Database/Repositories/Moderation/Access/UserBlackListRepository.cs
@@ -1,14 +1,18 @@
+using Dapper;
+using LeokaEstetica.Platform.Base.Abstractions.Connection;
+using LeokaEstetica.Platform.Base.Abstractions.Repositories.Base;
 using LeokaEstetica.Platform.Core.Data;
 using LeokaEstetica.Platform.Database.Abstractions.Moderation.Access;
 using LeokaEstetica.Platform.Models.Entities.Access;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace LeokaEstetica.Platform.Database.Repositories.Moderation.Access;
 
 /// <summary>
 /// Класс репозитория ЧС пользователей.
 /// </summary>
-internal sealed class UserBlackListRepository : IUserBlackListRepository
+internal sealed class UserBlackListRepository : BaseRepository, IUserBlackListRepository
 {
     private readonly PgContext _pgContext;
 
@@ -16,7 +20,9 @@ internal sealed class UserBlackListRepository : IUserBlackListRepository
     /// Конструктор.
     /// </summary>
     /// <param name="pgContext">Датаконтекст.</param>
-    public UserBlackListRepository(PgContext pgContext)
+    /// <param name="connectionProvider">Провайдер БД.</param>
+    public UserBlackListRepository(PgContext pgContext,
+        IConnectionProvider connectionProvider) : base(connectionProvider)
     {
         _pgContext = pgContext;
     }
@@ -25,29 +31,58 @@ internal sealed class UserBlackListRepository : IUserBlackListRepository
     /// Метод добавляет пользователя в ЧС.
     /// </summary>
     /// <param name="userId">Id пользователя.</param>
-    /// <param name="email">Почта для блока..</param>
+    /// <param name="email">Почта для блока.</param>
     /// <param name="phoneNumber">Номер телефона для блока.</param>
-    public async Task AddUserBlackListAsync(long userId, string email, string phoneNumber)
+    /// <param name="vkUserId">Id пользователя в системе ВКонтакте.</param>
+    public async Task AddUserBlackListAsync(long userId, string? email, string? phoneNumber, string? vkUserId)
     {
-        if (!string.IsNullOrEmpty(email))
+        using var connection = await ConnectionProvider.GetConnectionAsync();
+        using var transaction = connection.BeginTransaction(IsolationLevel.ReadCommitted);
+
+        var parameters = new DynamicParameters();
+        parameters.Add("@userId", userId);
+        
+        try
         {
-            await _pgContext.UserEmailBlackList.AddAsync(new UserEmailBlackListEntity
+            if (!string.IsNullOrEmpty(email))
             {
-                UserId = userId,
-                Email = email
-            });
+                parameters.Add("@email", email);
+
+                var insertUserEmailBlackQuery = "INSERT INTO access.user_email_black_list (user_id, email) " +
+                                                "VALUES (@userId, @email)";
+                
+                await connection.ExecuteAsync(insertUserEmailBlackQuery, parameters);
+            }
+
+            if (!string.IsNullOrEmpty(phoneNumber))
+            { 
+                parameters.Add("@phoneNumber", phoneNumber);
+
+                var insertUserPhoneNumberBlackQuery = "INSERT INTO access.user_phone_black_list (user_id, phone_number) " +
+                                                      "VALUES (@userid, @phoneNumber)";
+
+                await connection.ExecuteAsync(insertUserPhoneNumberBlackQuery, parameters);
+            }
+
+            if (!string.IsNullOrEmpty(vkUserId))
+            {
+                parameters.Add("@vkUserId", vkUserId);
+
+                var insertUserVkBlackQuery = "INSERT INTO access.user_vk_black_list (user_id, vk_user_id) " +
+                                             "VALUES (@userId, @vkUserId)";
+
+                await connection.ExecuteAsync(insertUserVkBlackQuery, parameters);
+            }
+
+            transaction.Commit();
         }
 
-        if (!string.IsNullOrEmpty(phoneNumber))
+        catch
         {
-            await _pgContext.UserPhoneBlackList.AddAsync(new UserPhoneBlackListEntity
-            {
-                UserId = userId,
-                PhoneNumber = phoneNumber
-            });
-        }
+            transaction.Rollback();
 
-        await _pgContext.SaveChangesAsync();
+            throw;
+        }
     }
 
     /// <summary>
