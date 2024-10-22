edba2f9aa303eb7d7002f5f8e5329cfae5deaf9f
Hihiz <public@mail.ru>
2024-10-22 21:34:54 +0300

    #40288389 Добавить входную модель удаления из ЧС

---
+++ LeokaEstetica.Platform.CallCenter/Models/Dto/Input/Access/RemoveUserBlackListInput.cs
@@ -0,0 +1,28 @@
+﻿namespace LeokaEstetica.Platform.CallCenter.Models.Dto.Input.Access
+{
+    /// <summary>
+    /// Класс входной модели удаления пользователя из ЧС.
+    /// </summary>
+    public class RemoveUserBlackListInput
+    {
+        /// <summary>
+        /// Id пользователя.
+        /// </summary>
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// Почта.
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// Номер телефона.
+        /// </summary>
+        public string? PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Id пользователя в системе ВКонтакте.
+        /// </summary>
+        public long? VkUserId { get; set; }
+    }
+}
