c5617574e08acf32dc5959c827df186131a0483c
SergeySukhatsky <88921698+SergeySuhatsky@users.noreply.github.com>
2024-10-23 21:28:12 +0300

    Добавил предотвращение отклонение резюме без внесения замечаний

--- LeokaEstetica.Platform.CallCenter/Consts/RemarkConst.cs
+++ LeokaEstetica.Platform.CallCenter/Consts/RemarkConst.cs
@@ -7,4 +7,7 @@ public static class RemarkConst
 {
     public const string SEND_PROJECT_REMARKS_WARNING = "Замечания не были внесены. Для отправки замечаний сначала " +
                                                        "они должны быть внесены.";
+
+    public const string REJECT_RESUME_REMARKS_WARNING = "Замечания не были внесены. Для отклонения анкеты требуется " +
+                                                    "внести замечания";
 }
